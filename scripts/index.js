15c120bd3298e495a65682a64b8911f1e191ec8a
Pavel Makarov <mr.makarovpavel@mail.ru>
2023-02-03 15:24:02 +0500

    fix: 2 итерация

--- scripts/index.js
+++ scripts/index.js
@@ -3,38 +3,29 @@ let closeButton = document.querySelector(".popup__button-close");
 let popup = document.querySelector(".popup");
 let name = document.querySelector(".profile__name");
 let description = document.querySelector(".profile__description");
-let inputName = document.querySelector('.popup__input_type_name');
-let inputDescription = document.querySelector('.popup__input_type_description');
-let formElement = document.querySelector('.popup__form');
-
-
+let inputName = document.querySelector(".popup__input_type_name");
+let inputDescription = document.querySelector(".popup__input_type_description");
+let formElement = document.querySelector(".popup__form");
 
 function opened() {
-popup.classList.add("popup_opened");
-inputName.value = name.textContent;
-inputDescription.value = description.textContent;
-};
+  popup.classList.add("popup_opened");
+  inputName.value = name.textContent;
+  inputDescription.value = description.textContent;
+}
 
 function close() {
   popup.classList.remove("popup_opened");
-};
+}
 
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-function handleFormSubmit (evt) {
-evt.preventDefault();
+  name.textContent = inputName.value;
+  description.textContent = inputDescription.value;
 
-name.textContent = inputName.value;
-description.textContent = inputDescription.value;
-
-return close();
-};
+  close();
+}
 
 editButton.addEventListener("click", opened);
 closeButton.addEventListener("click", close);
-addEventListener('submit', handleFormSubmit);
-
-
-
-
-
-
+addEventListener("submit", handleFormSubmit);
