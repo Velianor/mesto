ce9892525395af94efeefb62757d148c9001f123
Pavel Makarov <mr.makarovpavel@mail.ru>
2023-04-28 00:35:26 +0500

    dev: новые попапы, аватарка

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { Section } from "./scripts/Section.js";
 import { PopupWithImage } from "./scripts/PopupWithImage.js";
 import { PopupWithForm } from "./scripts/PopupWithForm.js";
 import { UserInfo } from "./scripts/UserInfo.js";
+import { Api } from "./scripts/Api.js";
+
+
 
 const buttonEdit = document.querySelector(".profile__edit-button");
 const buttonAdd = document.querySelector(".profile__add-button");
@@ -21,11 +24,21 @@ const cardNewForm = document.querySelector('form[name="newCardForm"]');
 const profileValidator = new FormValidator(validationConfig, profileForm);
 const cardValidator = new FormValidator(validationConfig, cardNewForm);
 const popupImageContainer = new PopupWithImage(".popup_type_image-popup");
-const userInfo = new UserInfo(".profile__name", ".profile__description");
+const userInfo = new UserInfo(".profile__name", ".profile__description", ".profile-avatar");
 
 const popupProfile = new PopupWithForm(".popup_type_profile", {
   handleSubmitForm: (data) => {
     userInfo.setUserInfo(data.name, data.info);
+  },
+});
+
+let myId = '';
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-64',
+  headers: {
+    authorization: '96377036-74fb-47dd-ae26-39fa8fb3f7f0',
+    'content-type': 'application/json'
   },
 });
 
